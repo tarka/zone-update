0b2e64e8e2bd50679ad1c2fe311e19500552568e
Steve <tarkasteve@gmail.com>
2025-09-13 15:50:56 +1000

    Initial working gandi impl.

--- src/gandi/mod.rs
+++ src/gandi/mod.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 // dns-edit: DNS provider update utilities
 // Copyright (C) 2025 tarkasteve@gmail.com
 //
@@ -22,134 +24,148 @@ use tracing::{error, info, warn};
 
 use types::{Record, RecordUpdate};
 
-use crate::{errors::{Error, Result}, http};
+use crate::{errors::{Error, Result}, http, Config, DnsProvider};
 
 const API_HOST: &str = "api.gandi.net";
 const API_BASE: &str = "/v5/livedns";
 
-fn get_auth() -> Result<String> {
-    let config = config::get_config()?;
-    let auth = if let Some(key) = &config.gandi_api_key {
-        format!("Apikey {key}")
-    } else if let Some(key) = &config.gandi_pat_key {
-        format!("Bearer {key}")
-    } else {
-        error!("No Gandi key set");
-        return Err(Error::AuthError("No Gandi key set".to_string()));
-    };
-    Ok(auth)
+pub enum Auth {
+    ApiKey(String),
+    PatKey(String),
 }
 
-#[allow(dead_code)]
-pub async fn get_records(domain: &str) -> Result<Vec<Record>> {
-    let url = format!("{API_BASE}/domains/{domain}/records");
-    let recs = http::get::<Vec<Record>, types::Error>(API_HOST, &url, Some(get_auth()?)).await?
-        .unwrap_or(vec![]);
-    Ok(recs)
+impl Auth {
+    fn get_header(&self) -> String {
+        match self {
+            Auth::ApiKey(key) => format!("Apikey {key}"),
+            Auth::PatKey(key) => format!("Bearer {key}"),
+        }
+    }
 }
 
-pub async fn get_host_ipv4(domain: &str, host: &str) -> Result<Option<Ipv4Addr>> {
-    let url = format!("{API_BASE}/domains/{domain}/records/{host}/A");
-    let rec: Record = match http::get::<Record, types::Error>(API_HOST, &url, Some(get_auth()?)).await? {
-        Some(rec) => rec,
-        None => return Ok(None)
-    };
 
-    let nr = rec.rrset_values.len();
+struct Gandi {
+    config: Config,
+    auth: Auth,
+}
 
-    // FIXME: Assumes no or single address (which probably makes sense
-    // for DDNS, but may cause issues with malformed zones.
-    if nr > 1 {
-        error!("Returned number of IPs is {}, should be 1", nr);
-        return Err(Error::UnexpectedRecord(format!("Returned number of IPs is {nr}, should be 1")));
-    } else if nr == 0 {
-        warn!("No IP returned for {host}, continuing");
-        return Ok(None);
+impl Gandi {
+    fn new(config: Config, auth: Auth) -> Self {
+        Gandi {
+            config,
+            auth,
+        }
+    }
+}
+
+impl DnsProvider for Gandi {
+
+    async fn get_v4_record(&self, host: &str) -> Result<Option<Ipv4Addr>> {
+        let url = format!("{API_BASE}/domains/{}/records/{host}/A", self.config.domain);
+        let auth = self.auth.get_header();
+        let rec: Record = match http::get::<Record, types::Error>(API_HOST, &url, Some(auth)).await? {
+            Some(rec) => rec,
+            None => return Ok(None)
+        };
+
+        let nr = rec.rrset_values.len();
+
+        // FIXME: Assumes no or single address (which probably makes sense
+        // for DDNS, but may cause issues with malformed zones.
+        if nr > 1 {
+            error!("Returned number of IPs is {}, should be 1", nr);
+            return Err(Error::UnexpectedRecord(format!("Returned number of IPs is {nr}, should be 1")));
+        } else if nr == 0 {
+            warn!("No IP returned for {host}, continuing");
+            return Ok(None);
+        }
+
+        let ip = rec.rrset_values[0].parse()?;
+        Ok(Some(ip))
+
     }
 
-    let ip = rec.rrset_values[0].parse()?;
-    Ok(Some(ip))
-}
+    async fn set_v4_record(&self, host: &str, ip: &Ipv4Addr) -> Result<()> {
+        let url = format!("{API_BASE}/domains/{}/records/{host}/A", self.config.domain);
+        let auth = self.auth.get_header();
 
-pub async fn set_host_ipv4(domain: &str, host: &str, ip: &Ipv4Addr) -> Result<()> {
-    let url = format!("{API_BASE}/domains/{domain}/records/{host}/A");
+        let update = RecordUpdate {
+            rrset_values: vec![ip.to_string()],
+            rrset_ttl: Some(300),
+        };
+        if self.config.dry_run {
+            info!("DRY-RUN: Would have sent {update:?} to {url}");
+            return Ok(())
+        }
+        http::put::<RecordUpdate, types::Error>(API_HOST, &url, Some(auth), &update).await?;
+        Ok(())
 
-    let update = RecordUpdate {
-        rrset_values: vec![ip.to_string()],
-        rrset_ttl: Some(300),
-    };
-    if config::get_config()?.dry_run.is_some_and(|b| b) {
-        info!("DRY-RUN: Would have sent {update:?} to {url}");
-        return Ok(())
     }
-    http::put::<RecordUpdate, types::Error>(API_HOST, &url, Some(get_auth()?), &update).await?;
-    Ok(())
+
 }
 
 #[cfg(test)]
 mod tests {
+    use std::env;
+
     use super::*;
     use macro_rules_attribute::apply;
     use smol_macros::test;
-    use temp_env::async_with_vars;
     use tracing_test::traced_test;
 
-    #[apply(test!)]
-    #[traced_test]
-    #[cfg_attr(not(feature = "test_gandi"), ignore = "Gandi API test")]
-    async fn test_fetch_records() -> Result<()> {
-        async_with_vars([("NLDDNS_CONFIG", Some("config.toml"))], async  {
-            let recs = get_records("haltcondition.net").await?;
-            assert!(recs.len() > 0);
-            Ok(())
-        }).await
-    }
+    fn get_client() -> Gandi {
+        let auth = if let Some(key) = env::var("GANDI_APIKEY").ok() {
+            Auth::ApiKey(key)
+        } else if let Some(key) = env::var("GANDI_PATKEY").ok() {
+            Auth::PatKey(key)
+        } else {
+            panic!("No Gandi auth key set");
+        };
 
-    #[apply(test!)]
-    #[traced_test]
-    #[cfg_attr(not(feature = "test_gandi"), ignore = "Gandi API test")]
-    async fn test_fetch_records_error() -> Result<()> {
-        async_with_vars([("NLDDNS_CONFIG", Some("config.toml"))], async  {
-            let recs = get_records("not.a.real.domain.net").await?;
-            assert!(recs.is_empty());
-            Ok(())
-        }).await
+        let config = Config {
+            domain: "haltcondition.net".to_string(),
+            dry_run: false,
+        };
+
+        Gandi {
+            config,
+            auth,
+        }
+
+
     }
 
     #[apply(test!)]
     #[traced_test]
     #[cfg_attr(not(feature = "test_gandi"), ignore = "Gandi API test")]
     async fn test_fetch_ipv4() -> Result<()> {
-        async_with_vars([("NLDDNS_CONFIG", Some("config.toml"))], async  {
-            let ip = get_host_ipv4("haltcondition.net", "janus").await?;
-            assert!(ip.is_some());
-            assert_eq!(Ipv4Addr::new(192,168,42,1), ip.unwrap());
-            Ok(())
-        }).await
+        let client = get_client();
+        let ip = client.get_v4_record("janus").await?;
+        assert!(ip.is_some());
+        assert_eq!(Ipv4Addr::new(192,168,42,1), ip.unwrap());
+        Ok(())
     }
 
     #[apply(test!)]
     #[traced_test]
     #[cfg_attr(not(feature = "test_gandi"), ignore = "Gandi API test")]
     async fn test_update_ipv4() -> Result<()> {
-        async_with_vars([("NLDDNS_CONFIG", Some("config.toml"))], async  {
-            let cur = get_host_ipv4("haltcondition.net", "test").await?
-                .unwrap_or(Ipv4Addr::new(1,1,1,1));
-            let next = cur.octets()[0]
-                .wrapping_add(1);
+        let client = get_client();
+        let cur = client.get_v4_record("test").await?
+            .unwrap_or(Ipv4Addr::new(1,1,1,1));
+        let next = cur.octets()[0]
+            .wrapping_add(1);
 
-            let nip = Ipv4Addr::new(next,next,next,next);
-            set_host_ipv4("haltcondition.net", "test", &nip).await?;
+        let nip = Ipv4Addr::new(next,next,next,next);
+        client.set_v4_record("test", &nip).await?;
 
-            let ip = get_host_ipv4("haltcondition.net", "test").await?;
-            if let Some(ip) = ip {
-                assert_eq!(nip, ip);
-            } else {
-                assert!(false, "No updated IP found");
-            }
-
-            Ok(())
-        }).await
+        let ip = client.get_v4_record("test").await?;
+        if let Some(ip) = ip {
+            assert_eq!(nip, ip);
+        } else {
+            assert!(false, "No updated IP found");
+        }
+        Ok(())
     }
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -17,4 +17,20 @@
 mod http;
 pub mod errors;
 
-//pub mod gandi;
+pub mod gandi;
+
+use std::net::Ipv4Addr;
+
+use crate::errors::Result;
+
+
+struct Config {
+    domain: String,
+    dry_run: bool,
+}
+
+#[allow(unused)]
+trait DnsProvider {
+    async fn get_v4_record(&self, host: &str) -> Result<Option<Ipv4Addr>>;
+    async fn set_v4_record(&self, host: &str, ip: &Ipv4Addr) -> Result<()>;
+}
